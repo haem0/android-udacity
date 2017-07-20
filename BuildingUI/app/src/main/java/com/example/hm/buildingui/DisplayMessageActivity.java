fcd5a93d5da4dae23751e35264dc6a838ba23d3e
haem0 <h.metrycki@gmail.com>
2017-07-20 18:33:18 +0200

    create

---
+++ BuildingUI/app/src/main/java/com/example/hm/buildingui/DisplayMessageActivity.java
@@ -0,0 +1,24 @@
+package com.example.hm.buildingui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("EXTRA_MESSAGE");
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(message);
+
+
+
+    }
+}
